9687eb603aa7158e8d8fef581e3d0a023ae8164e
mroberts <mroberts@cargurus.com>
2023-06-28 09:59:21 -0400

    fix pyenv and homebrew

--- install_vms.sh
+++ install_vms.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # homebrew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 
 # jenv
